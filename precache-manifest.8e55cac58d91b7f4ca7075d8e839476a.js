bf614dfeb93a6cbf513360a0b5c0e0e62782d398
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-06-26 22:41:37 +0100

    Updates

---
+++ precache-manifest.8e55cac58d91b7f4ca7075d8e839476a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf341fc4302022b4f37e",
+    "url": "/robofriendsApp/static/css/main.d0723ac9.chunk.css"
+  },
+  {
+    "revision": "bf341fc4302022b4f37e",
+    "url": "/robofriendsApp/static/js/main.bf341fc4.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriendsApp/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "3da5dfa108a7a7e714ed",
+    "url": "/robofriendsApp/static/css/2.d4548148.chunk.css"
+  },
+  {
+    "revision": "3da5dfa108a7a7e714ed",
+    "url": "/robofriendsApp/static/js/2.3da5dfa1.chunk.js"
+  },
+  {
+    "revision": "b7e65a407211b3ced323",
+    "url": "/robofriendsApp/static/js/runtime~main.b7e65a40.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsApp/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "39085d63bbd4b03eaa3733a835da38fa",
+    "url": "/robofriendsApp/index.html"
+  }
+];
